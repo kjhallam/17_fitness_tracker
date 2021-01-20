da9bad52b1dc5359bf3092a9d4ed7958d10f553c
KHallam <kjhallam321@gmail.com>
2021-01-19 18:24:34 -0600

    added workout/updated index/server update

---
+++ models/Workout.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+const WorkoutSchema = new Schema({
+//name, type, weight, sets, reps, and duration of exercise
+day: {
+    type: Date,
+    default: new Date().setDate(new Date().getDate()),
+},
+exercise: [{
+    type: {
+        type: String,
+        // trim: true,
+    },
+    name: {
+        type: String,
+        // trim: true,
+    },
+    weight: {
+        type: Number,
+        default: 0,
+    },
+    reps: {
+        type: Number,
+        default: 0,
+    },
+    sets: {
+        type: Number,
+        default: 0,
+    },
+    duration: Number,
+    distance: {
+        type: Number,
+        default: 0,
+    }
+}],
+totalDuration: {
+    type: Number,
+    // default: 0,
+}
+   
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema)
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose')
 
-const Exercise = require('./models/Exercise')
-
-
-
+app.use(logger('dev'));
 
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/exercise_tracker',
- {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
      useUnifiedTopology: true, 
-     useNewUrlParser: true
+     useNewUrlParser: true,
     }
-)
+);
 
-app.get('/api/test', (req, res) => {
-    Exercise.find({}, function(err, data){
-        if(err)console.log(err)
-        res.json(data)
-    })
-})
-
-app.post('/api/test', (req,res) => {
-   
-
-    Exercise.create(req.body, function(err, data){
-        if(err) {
-            console.log(err)
-        }
-        res.json(data)
-        console.log('success --> ', newExercise)
-    })
-
-})
-
-
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 app.listen(PORT, function() {
     console.log(`Listening on PORT ${PORT}`)
-})
+});
